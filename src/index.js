81591276fd91faf236a452b42ee9b634507af770
Diveana Inciarte <dinciarte25@gmail.com>
2023-09-20 10:09:54 -0300

    Subo el detalle para revisión de sprint. Ya cuento con el wordcount, pero a medias porque necesito hacer que no cuente espacios. También, cuento con la función para el button reset.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import analyzer from './analyzer.js';
 
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const output1 = document.querySelector(".mi-output1");
+const textArea = document.querySelector("textarea");
+const botonReset = document.getElementById("reset-button")
+
+textArea.addEventListener("input", () => {
+    const resultadoPalabras = analyzer.getWordCount(textArea.value);
+    output1.innerHTML = "Palabras " + resultadoPalabras ;
+
+  }); 
+
+botonReset.addEventListener("click", () => {
+    console.log(textArea.value);
+    textArea.value = " ";
+    output1.innerHTML = "Palabras ";
+}) 
