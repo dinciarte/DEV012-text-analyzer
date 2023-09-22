eafb99141336dcf929f8bb2ef93939519eb7f85b
Diveana Inciarte <dinciarte25@gmail.com>
2023-09-22 16:25:24 -0300

    Funciones 3, 4 y 6 listas.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,26 +1,68 @@
 const analyzer = {
   getWordCount: (text) => {
-    const palabras = text.split(" ");
-    const palabrasSinEspacio = palabras.filter(
-      (palabra) => palabra.length !== 0
-    );
-    return palabrasSinEspacio.length;
+    if (text === "") {
+      return 0;
+    } else {
+      const palabras = text.split(" ");
+      const palabrasSinEspacio = palabras.filter(
+        (palabra) => palabra.length !== 0
+      );
+      return palabrasSinEspacio.length;
+    }
   },
+
   getCharacterCount: (text) => {
-    return text.length;
+    if (text === "") {
+      return 0;
+    } else {
+      return text.length;
+    }
   },
-  //   getCharacterCountExcludingSpaces: (text) => {
-  //     TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  //   },
-  //   getAverageWordLength: (text) => {
-  //     TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  //   },
+
+  getCharacterCountExcludingSpaces: (text) => {
+    if (text === "") {
+      return 0;
+    } else {
+      const regex = /[a-zA-Z0-9]/g;
+      const caracteresSinEspacio = text.match(regex) || [].length;
+      return caracteresSinEspacio.length;
+    }
+  },
+
+  getAverageWordLength: (text) => {
+    const palabrasCadena = text.split(" ");
+    const palabrasValidas = palabrasCadena.filter(
+      (palabraCadena) => palabraCadena.trim().length > 0
+    );
+
+    if (palabrasValidas.length === 0) {
+      return 0;
+    } else {
+      const longitudPalabras = palabrasValidas.reduce(
+        (sumaPalabra, palabraCadena) => sumaPalabra + palabraCadena.length,
+        0
+      );
+      const longituPromedioPalabras = longitudPalabras / palabrasValidas.length;
+      const longituPromedioPalabrasRedondeadas = parseFloat(
+        longituPromedioPalabras
+      ).toFixed(2);
+      return longituPromedioPalabrasRedondeadas;
+    }
+  },
   //   getNumberCount: (text) => {
   //     TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   //   },
-  //   getNumberSum: (text) => {
-  //     TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  //   },
+  getNumberSum: (text) => {
+      let suma = 0;
+      const numerosArray = text.split(' ');
+      for (let i = 0; i < numerosArray.length; i++) {
+        const numero = parseFloat(numerosArray[i]);
+        if (typeof numero === 'number') {
+          suma += numero;
+        }
+      }
+      return "Números: " + " " + suma ; 
+    }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -5,21 +5,30 @@ import analyzer from './analyzer.js';
 
 const output1 = document.querySelector("ul>li:nth-child(1)")
 const output2 = document.querySelector("ul>li:nth-child(2)") 
+const output3 = document.querySelector("ul>li:nth-child(3)")
+const output4 = document.querySelector("ul>li:nth-child(6)")
 const textArea = document.querySelector("textarea");
 const botonReset = document.getElementById("reset-button")
 
-textArea.addEventListener("input", () => {
-    const resultadoPalabras = analyzer.getWordCount(textArea.value);
-    output1.innerHTML = "Palabras: " + resultadoPalabras;
+const updateList = () => {
 
-    const resultadoCaracter = analyzer.getCharacterCount(textArea.value);
-    output2.innerHTML = "Caracteres: " + resultadoCaracter;
+  const resultadoPalabras = analyzer.getWordCount(textArea.value);
+  output1.innerHTML = "Palabras: " + resultadoPalabras;
 
+  const resultadoCaracter = analyzer.getCharacterCount(textArea.value);
+  output2.innerHTML = "Caracteres: " + resultadoCaracter;
 
-  }); 
+  const resultadoCaracterSinEspacio = analyzer.getCharacterCountExcludingSpaces(textArea.value);
+  output3.innerHTML = "Caracteres S/E: " + resultadoCaracterSinEspacio;
+
+  const resultadoPromedioPalabras = analyzer.getAverageWordLength(textArea.value);
+  output4.innerHTML  = "Longitud promedio: " + resultadoPromedioPalabras;
+
+}
+
+textArea.addEventListener("input", updateList); 
 
 botonReset.addEventListener("click", () => {
     textArea.value = "";
-    output1.innerHTML = "Palabras: 0";
-    output2.innerHTML= "Caracteres: 0"
-}) 
+    updateList();
+});
